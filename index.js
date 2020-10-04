df553d2e3d616ca05b02248499657cfd649bf701
H.tueej <h.tueej@Htueejs-MacBook-Pro.local>
2020-10-04 09:34:48 +0700

    innit message

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+    khởi tạo git: git init
+    kết nối project với repo: git remote add origin url-repo
+    kiểm tra trạng thái của source: git status
+        + file màu đỏ: file đã thay đổi nhưng chưa save (working copy)
+        + file màu xanh: file đã thay đổi và đã được save(staging area);
+    Save những file đã thay đổi(ửoking copy => staging area); git add file-name hoặc .(tất cả các file);
+*/
