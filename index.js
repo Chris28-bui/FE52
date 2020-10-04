e942552177e884d04ca7ab99b68c7a2a5686f942
Tue Bui <tuebui2806@gmail.com>
2020-10-04 09:51:16 +0700

    second commit

--- index.js
+++ index.js
@@ -5,4 +5,10 @@
         + file màu đỏ: file đã thay đổi nhưng chưa save (working copy)
         + file màu xanh: file đã thay đổi và đã được save(staging area);
     Save những file đã thay đổi(ửoking copy => staging area); git add file-name hoặc .(tất cả các file);
+    commit những thay đổi (stagin area => local repo ): git commit -m "message"
+    Lần đầu tiên xài git cần chạy 2 lệnh:
+    git congig --global user.name "name"
+    git config ==global user.email "email"
+    đẩy code lên server (local repo => remote repository): git push -u origin master
+    lần thứ 2 trở đi thì git push
 */
